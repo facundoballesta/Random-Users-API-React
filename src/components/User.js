4c857dd13d53b48f1d8dd333bbc87511d9ac1ebe
Facundo Ballesteros <facundoballesteros01@hotmail.com>
2021-04-11 21:27:26 -0300

    profile info

--- src/components/User.js
+++ src/components/User.js
@@ -32,6 +32,15 @@ export default function User(){
                 <h1>{user.name.first} {user.name.last}</h1>
                 <p>{user.email}</p>
                 <hr/>
+                <div className="container bg-white rounded text-dark text-start">
+                    <h2>Info</h2>
+                    <ul>
+                        <li>Age: {user.dob.age}</li>
+                        <li>Gender: {user.gender}</li>
+                        <li>Address: {user.location.street.number}, {user.location.street.name}, {user.location.city}, {user.location.state}, {user.location.country}</li>
+                        <li>Phone: {user.phone}</li>
+                    </ul>
+                </div>
             </div>
         );
     }
